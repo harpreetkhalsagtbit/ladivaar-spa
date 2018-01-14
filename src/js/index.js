d18f5ae5b3cfaa5189ac623056985b7c12428fba
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2018-01-14 13:36:34 +0530

    Fix: invalid ang no issue, fix: hide tatkta on focus out

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,17 @@ $( document ).ready(function() {
         var ang = window.location.hash.match(/\d{1,4}$/);
         if(ang && ang.length) {
             ang--;
+            if(ang > 1430) {
+                window.location.hash = "ang/" + 1430
+            }
+            if(ang < 0) {
+                window.location.hash = "ang/" + 1
+            }
+        } else {
+            window.location.hash = "ang/" + 1
+        }
+
+        if(ang < 1430 && ang >= 0) {
             window.angNo = ang;
 
             var _angData = data[window.angNo];
@@ -30,20 +41,30 @@ $( document ).ready(function() {
 
     var _changeInterval = null;
     var mouseout = true;
-    $('body').on("mousemove",function() {
-        // wait untill user type in something
-        // Don't let call setInterval - clear it, user is still typing
-        clearInterval(_changeInterval)
-        if(mouseout) {
-            _changeInterval = setInterval(function() {
-                // Typing finished, now you can Do whatever after 2 sec
-                clearInterval(_changeInterval)
-                $(".toolbar").hide()
-            }, 2000);
-            $(".toolbar").show()
-        }
-    })
+    // $('body, tatkra').on("mousemove",function() {
+    //     // wait untill user type in something
+    //     // Don't let call setInterval - clear it, user is still typing
+    //     clearInterval(_changeInterval)
+    //     if(mouseout) {
+    //         _changeInterval = setInterval(function() {
+    //             // Typing finished, now you can Do whatever after 2 sec
+    //             clearInterval(_changeInterval)
+    //             $(".toolbar").hide()
+    //         }, 2000);
+    //         $(".toolbar").show()
+    //     }
+    // })
+                $(".toolbar").show()
     
+    $(".flex-container").on("click", function() {
+        $(".tatkra").removeClass("tatkra_hover")
+    })
+
+    $(".tatkra").on("mouseleave", function() {
+        console.log("outttttt ta")
+        $(".tatkra").removeClass("tatkra_hover")
+    })
+
     // $('.guru_granth_sahib_baani').on("mousemove",function() {
     //     $(".tatkra").removeClass("tatkra_hover")
     //     window.location.hash = "ang/" + (window.angNo+1);
@@ -68,54 +89,69 @@ $( document ).ready(function() {
     $(".goToAngButton").on("click", function() {
         var _ang = parseInt($(".angNoInput").val());
         if (!isNaN(_ang) && _ang > 0 && _ang <= 1430) {
-            window.location.hash = "ang_" + _ang
+            window.location.hash = "ang/" + _ang
         }
         $(".angNoInput").val("")
     })
     
     $(".leftHandle").on("click", function() {
-        // dont confuse with swipe right - swipe to right means
-        // move prev
-        swipeRight();
+        if(window.angNo - 1 >= 0) {
+            // dont confuse with swipe right - swipe to right means
+            // move prev
+            swipeRight();
+        }
     })
 
     $(".rightHandle").on("click", function() {
-        // dont confuse with swipe left - swipe to left means
-        // move next
-        swipeLeft()
+        if(window.angNo + 1 < 1430) {
+            // dont confuse with swipe left - swipe to left means
+            // move next
+            swipeLeft()
+        }
     })
 
     $(window).on('hashchange load', function() {
-        console.log("haschange", window.location.hash)
-        // hack
-        $(".nitnem_listX").css("display", "none");
-        if (window.location.hash == "") {
-            $(".tatkra").removeClass("tatkra_hover")
-            $(".guru_granth_sahib_baani").hide();
-            $(".rightHandle").hide();
-            $(".leftHandle").hide();
-        } else if (window.location.hash == "#tatkra") {
-            $(".tatkra").addClass("tatkra_hover")
-            // $(".guru_granth_sahib_baani").hide();
-            // $(".rightHandle").hide();
-            // $(".leftHandle").hide();
-        } else if (window.location.hash.match(/\d{1,4}$/)) {
-            $(".tatkra").removeClass("tatkra_hover")
-            $(".guru_granth_sahib_baani").show();
-            $(".rightHandle").show()
-            $(".leftHandle").show()
-                    var ang = window.location.hash.match(/\d{1,4}$/);
-            if(ang && ang.length) {
-                ang--;
-                window.angNo = ang;
-                console.log(window.angNo)
+        // console.log("haschange", window.location.hash)
+        var ang = window.location.hash.match(/\d{1,4}$/);
+        if(ang && ang.length) {
+            ang--;
+            if(ang > 1430) {
+                window.location.hash = "ang/" + 1430
             }
-            var _angData = data[window.angNo];
-            var _baani = _angData.baani
-                // baani_container
-            let center = $(".list-group-center-on-next").length?$(".list-group-center-on-next"):$(".list-group-center-on-prev")
-            ladivaarGenerator(_baani, center, false);
-            $(".nitnem_listX").show();
+            if(ang < 0) {
+                window.location.hash = "ang/" + 1
+            }
+        } else {
+            window.location.hash = "ang/" + 1
+        }
+
+        if(ang < 1430 && ang >= 0) {
+            // hack
+            $(".nitnem_listX").css("display", "none");
+            if (window.location.hash == "") {
+                $(".tatkra").removeClass("tatkra_hover")
+                $(".guru_granth_sahib_baani").hide();
+                $(".rightHandle").hide();
+                $(".leftHandle").hide();
+            } else if (window.location.hash == "#tatkra") {
+                $(".tatkra").addClass("tatkra_hover")
+                // $(".guru_granth_sahib_baani").hide();
+                // $(".rightHandle").hide();
+                // $(".leftHandle").hide();
+            } else if (window.location.hash.match(/\d{1,4}$/)) {
+                $(".tatkra").removeClass("tatkra_hover")
+                $(".guru_granth_sahib_baani").show();
+                $(".rightHandle").show()
+                $(".leftHandle").show()
+
+                window.angNo = ang;
+                var _angData = data[window.angNo];
+                var _baani = _angData.baani
+                    // baani_container
+                let center = $(".list-group-center-on-next").length?$(".list-group-center-on-next"):$(".list-group-center-on-prev")
+                ladivaarGenerator(_baani, center, false);
+                $(".nitnem_listX").show();
+            }
         }
     });
 
